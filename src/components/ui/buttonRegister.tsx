9d99921b4919058f8aef4f1e4205ab2e8ed9bf06
phuong171105 <phuong1711tn@gmail.com>
2025-03-21 11:25:09 +0700

    update

--- src/components/ui/buttonRegister.tsx
+++ src/components/ui/buttonRegister.tsx
@@ -1,9 +1,18 @@
 import { FaCircleArrowUp } from "react-icons/fa6";
 
-export default function ButtonRegister() {
+export function ButtonRegisterDefault() {
     return(
-        <button className="flex items-center justify-between bg-color-1-500 rounded-full w-auto transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:opacity-75">
+        <button className="mb-[2rem] hidden lg:block lg:mb-[0rem] lg:flex items-center justify-between bg-color-1-500 rounded-full w-auto transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:opacity-75">
             <p className="text-white pl-[2rem]">Đăng ký dùng thử miễn phí</p>
+            <FaCircleArrowUp className=" text-2xl m-[1rem] rotate-45 text-white hover:rotate-90 hover:delay-150 duration-300  hover:ease-in-out  " />
+        </button>
+    );
+}
+
+export function ButtonRegisterVariant1() {
+    return(
+        <button className="mb-[0rem] sm:ml-[2rem] lg:hidden lg:mb-[0rem] flex items-center justify-center bg-color-1-500 rounded-full w-auto transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:opacity-75">
+            <p className="text-white pl-[2rem]">Đăng ký</p>
             <FaCircleArrowUp className=" text-2xl m-[1rem] rotate-45 text-white hover:rotate-90 hover:delay-150 duration-300  hover:ease-in-out  " />
         </button>
     );
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,7 +1,7 @@
 import {Header} from "./_components/Header";
 import Footer from "./_components/footer";
 import { Helmet } from 'react-helmet';
-import ButtonRegister from "../components/ui/buttonRegister";
+import {ButtonRegisterDefault, ButtonRegisterVariant1} from "../components/ui/buttonRegister";
 
 //img
 import picture1 from "../assets/picture1.png";
@@ -29,22 +29,23 @@ export default function Homepage() {
         {/* section 1 */}
         <section id="hero-section" >
           <div className="grid-container">
-            <div className="item1" >  
+            <div className="item1 hidden lg:block" >  
               <img src={picture1}/>            
             </div>
             <div className="item2">
               <div className=" xl:flex items-start pb-[2rem]">
-                <p className="text-6xl text-center">Bứt phá học tập cùng </p>
-                <div className="text-Aithenos">
-                  <p>Aithenos</p>
+                <p className="text-6xl text-center font-semibold">Bứt phá học tập cùng </p>
+                <div className="text-Aithenos md:mx-[25%] lg:mx-0">
+                  <p className="mt-2 lg:mt-0">Aithenos</p>
                   <img src={highlight}/>
                 </div>
               </div>
-              <p className="text-center py-[2rem] text-3xl text-custom-gray-primary ">Trải nghiệm tự học cá nhân hóa và mạnh mẽ với AI tiên tiến</p>
+              <p className="text-center py-[2rem] text-3xl text-gray ">Trải nghiệm tự học cá nhân hóa và mạnh mẽ với AI tiên tiến</p>
               <p className="text-center text-color-1-500 text-xl pb-[2rem]">Đừng lo học hành chậm chạp, Aithenos giúp tiết kiệm thời gian, học nhanh, đỗ cao – thử ngay!</p>
-              <div className="mt-[2rem] flex items-center justify-center">
-                <ButtonRegister />
-                <div className="max-w-[12rem] ml-[2rem] flex items-center justify-center gap-2">
+              <div className="mt-[2rem] mx-[25%] lg:mx-0 md:flex items-center justify-center">
+                <ButtonRegisterDefault />
+                <ButtonRegisterVariant1 />
+                <div className="max-w-[12rem] mb-[0rem] lg:mt-0  sm:ml-[2rem] flex items-center justify-center gap-2">
                 <button className="w-15 h-15 bg-white border border-color-1-500 rounded-full">
                   <BsTriangleFill className="m-[1rem] rotate-90 text-color-1-500"/>
                 </button>
@@ -52,7 +53,7 @@ export default function Homepage() {
                 </div>
               </div>
             </div>
-            <div> 
+            <div className="hidden lg:block"> 
               <img src={picture2} className="my-[4rem]" />
               <img src={arrow} />
             </div>
@@ -61,7 +62,17 @@ export default function Homepage() {
         {/* section 2 */}
         <section id="section2">
           <div className="container">
-
+            <div className="col-span-4 col-start-2 col-end-10 text-center">
+            <p className="mx-auto my-[2rem] font-extrabold text-color-1-500 text-6xl">Khám Phá Aithenos </p>
+            <div className="mx-[5%]">
+              <div className="border rounded-xl border-solid bg-white border-color-1-500 p-[2rem] mt-[2rem]">
+                <p>Aithenos là một hệ sinh thái tiên phong về giáo dục chuyển hóa, sử dụng sức mạnh của AI để mang đến trải nghiệm học tập đột phá cho giáo viên, học sinh, và phụ huynh tại Việt Nam. Với công nghệ tự học thông minh, lộ trình học thích ứng, và tốc độ tăng trưởng học tập vượt trội, chúng tôi giúp trẻ phát triển kỹ năng tự học, tích lũy các kỹ năng thiết yếu cho thế kỷ 21 như tư duy sáng tạo, giải quyết vấn đề, và kỹ năng số.</p>
+              </div>
+              <div className="border rounded-xl border-solid bg-white border-color-1-500 p-[2rem] mt-4">
+                <p>Chúng tôi hướng tới việc trở thành hệ sinh thái giáo dục chuyển hóa tiên phong tại Việt Nam, nơi mọi học sinh và giáo viên có thể tiếp cận các công cụ AI tiên tiến để tự học linh hoạt, thích ứng với nhu cầu cá nhân, và tăng tốc tiến bộ học tập. Aithenos không chỉ định hình tương lai giáo dục mà còn phù hợp với chiến lược phát triển bền vững của Chính phủ Việt Nam.</p>
+              </div>
+            </div>
+            </div>
           </div>
         </section>
         {/* section 3 */}
